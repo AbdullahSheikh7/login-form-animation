28e5b2da383671ce18b9f7b751dbd0aa8889b7b4
Abdullah <abdullahwindows10user@gmail.com>
2024-02-20 03:08:25 +0500

    Commit after making complete form for desktop and tabs only it is not responsive on mobile phones

---
+++ script.js
@@ -0,0 +1,4 @@
+function action() {
+    document.getElementsByClassName("container")[0].classList.toggle("signup")
+    document.getElementsByClassName("container")[0].classList.toggle("signin")
+}
